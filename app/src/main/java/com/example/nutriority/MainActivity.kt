928c5ba3ff8ff9d9938af2a8d7814f6e2276a48b
nexon <nexonjrsabanao@gmail.com>
2025-12-10 14:20:43 +0800

    version 1.1

--- app/src/main/java/com/example/nutriority/MainActivity.kt
+++ app/src/main/java/com/example/nutriority/MainActivity.kt
@@ -4,18 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-
-/**
- * MainActivity is the app's entry point.
- * Its ONLY responsibility is to display the startup navigation graph (`my_nav.xml`),
- * which begins with the SplashFragment. It does not handle any other UI or logic.
- */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //splash screen.
         installSplashScreen()
-        // Force to light mode
         super.onCreate(savedInstanceState)
 
         if (AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_NO) {
