edf05e011e981e70627c281593807ed61c849c92
Nexon Sabanao <nexonjrsabanao@gmail.com>
2025-12-21 16:49:59 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,13 +39,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
     sourceSets {
         getByName("main") {
             assets {
-                srcDirs("src\\main\\assets", "src\\main\\assets\\pictures")
+                srcDirs("src/main/assets", "src/main/assets/pictures")
             }
         }
     }
