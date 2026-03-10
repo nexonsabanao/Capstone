68dfee3d92d19c35714e6fa7bcbb48437741f64d
Nexon Sabanao <nexonjrsabanao@gmail.com>
2026-03-11 06:11:44 +0800

    update

--- app/src/main/java/com/example/nutriority/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/nutriority/data/local/AppDatabase.kt
@@ -26,7 +26,7 @@ import java.io.BufferedReader
 
 @Database(
     entities = [Meal::class, Workout::class, Article::class, Exercise::class, WorkoutLog::class, WorkoutExercise::class, WorkoutSessionLog::class, DailyMealLog::class],
-    version = 42,
+    version = 45,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
